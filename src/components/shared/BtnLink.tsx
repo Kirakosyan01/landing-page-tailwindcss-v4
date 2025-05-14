cc8291b19b927d1284d4d793eb49b62e8680af9a
Albert Kirakosyan <albert69kirakosyan@gmail.com>
2025-05-14 18:23:48 +0400

    landing page

---
+++ src/components/shared/BtnLink.tsx
@@ -0,0 +1,13 @@
+interface BtnLinkProps {
+    text: string;
+    href: string;
+    className?: string;
+}
+
+export const BtnLink = ({text, href, className=""}: BtnLinkProps) => {
+    return (
+        <a href={href} className={`px-6 py-3 rounded-full outline-none relative overflow-hidden border dark:bg-violet-600 cursor-pointer ${className}`} >
+            <span className="relative z-10 text-white">{text}</span>
+        </a>
+    )
+}
