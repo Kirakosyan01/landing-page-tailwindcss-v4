4f6db2a02da740abeab9d9e266486956ac6bbab6
Albert Kirakosyan <albert69kirakosyan@gmail.com>
2025-05-15 11:21:20 +0400

    debug

--- src/components/sections/Brands.tsx
+++ src/components/sections/Brands.tsx
@@ -24,7 +24,7 @@ export const Brands = () => {
               className="p-4 sm:p-5 rounded-xl bg-body border border-box-border group"
             >
               <img
-                src={`src/assets/logos/${logo}.png`}
+                src={`/logos/${logo}.png`}
                 alt={logo}
                 width={100}
                 height={60}
